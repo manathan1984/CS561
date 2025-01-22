fec4522d00e1e36f2ad3d366298d4619bf64a7b4
Manos Athanassoulis <manos.athanassoulis@gmail.com>
2025-01-22 14:43:05 -0500

    working on slides, on: ../fetch_slides.sh (deleted files: CAS-CS561-Class.pdf CAS-CS561-Class1.pdf CAS-CS561-Class10.pdf CAS-CS561-Class11.pdf CAS-CS561-Class12.pdf CAS-CS561-Class13.pdf CAS-CS561-Class14.pdf CAS-CS561-Class15.pdf CAS-CS561-Class16.pdf CAS-CS561-Class17.pdf CAS-CS561-Class18.pdf CAS-CS561-Class19.pdf CAS-CS561-Class2.pdf CAS-CS561-Class20.pdf CAS-CS561-Class21.pdf CAS-CS561-Class22.pdf CAS-CS561-Class23.pdf CAS-CS561-Class24.pdf CAS-CS561-Class25.pdf CAS-CS561-Class3.pdf CAS-CS561-Class4.pdf CAS-CS561-Class5.pdf CAS-CS561-Class6.pdf CAS-CS561-Class6old.pdf CAS-CS561-Class7.pdf CAS-CS561-Class8.pdf CAS-CS561-Class9.pdf)

--- fetch_slides.sh
+++ fetch_slides.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-cp ~/Dropbox/Professional/Professorship/0_CLASSES/CS561-Spring2022/Slides-PDF/*.pdf ./slides
+set -x
+cp ~/Dropbox/Professional/Professorship/W-Lab/CS561-Spring2025/slides-PDFs/*.pdf ./slides
+# cp ~/Dropbox/Professional/Professorship/0_CLASSES/CS561-Spring2025/Slides-PDF/*.pdf ./slides
+# cp  ~/Repos/cs460-660-materials/_cs660-slides-pdf/*.pdf slides/
+
+if [ $# -ne 0 ]; then
+    echo "Only copying, no pushing."
+    exit
+fi
 
 git pull
-git add slides/*pdf
-git commit -m "adding new slides"
+
+if [ $? -ne 0 ]; then
+    echo "Please ensure manually that pull resulted in a sane repo. Exiting ..."
+    exit
+fi
+git add slides/*
+git commit -m "CS660: adding new slides"
 git push
-
-
+set +x
